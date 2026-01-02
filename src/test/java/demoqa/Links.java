c03d5225c05f9df1b5a836654d8330a6f87ac5b4
SachinthaVidanagamage <sachinthavida@gmail.com>
2026-01-02 21:10:04 +0530

    create new Links.java class

---
+++ src/test/java/demoqa/Links.java
@@ -0,0 +1,16 @@
+package demoqa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Links {
+    WebDriver driver;
+
+
+    @Test
+    public void methodONe(){
+
+    }
+}
