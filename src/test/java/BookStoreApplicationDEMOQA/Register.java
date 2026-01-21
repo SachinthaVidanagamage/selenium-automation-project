7b128efed1a80ba67e807153b2f0265013fc611a
SachinthaVidanagamage <sachinthavida@gmail.com>
2026-01-21 21:30:59 +0530

    update Register

--- src/test/java/BookStoreApplicationDEMOQA/Register.java
+++ src/test/java/BookStoreApplicationDEMOQA/Register.java
@@ -20,9 +20,22 @@ public void beforeMethod(){
     driver.get("https://demoqa.com/login");
 }
 @Test
-public void methodForRegister(){
+public void methodForRegister() throws InterruptedException {
+
+    Thread.sleep(10000);
     driver.findElement(By.id("newUser")).click();
     driver.findElement(By.id("firstname")).sendKeys("Sachintha");
+    driver.findElement(By.id("lastname")).sendKeys("Lakshan");
+    driver.findElement(By.id("userName")).sendKeys("sachin_vidanagamage");
+    driver.findElement(By.id("password")).sendKeys("sachin123");
+    Thread.sleep(5000);
+    driver.findElement(By.id("gotologin")).click();
+
+    driver.findElement(By.id("userName")).sendKeys("sachin_vidanagamage");
+    driver.findElement(By.id("password")).sendKeys("sachin123");
+    Thread.sleep(8000);
+    driver.findElement(By.id("login")).click();
+
 
 }
 
@@ -30,8 +43,8 @@ public void methodForRegister(){
 
 @AfterClass
     public void afterClassMethod() throws InterruptedException {
-    Thread.sleep(2000);
-    driver.quit();
+//    Thread.sleep(4000);
+//    driver.quit();
 }
 
 
